11023e0ef288cb643ce28cd6274ff1ea6a8bb19a
Mason Namminga (he/him/his) <masonellisnamminga@gmail.com>
2023-07-03 14:37:05 -0700

    Summer 2023 Update Test

---
+++ masonellisnamminga-main/script/share.js
@@ -0,0 +1,23 @@
+const shareButton = document.querySelector(".share-button");
+const shareDialog = document.querySelector(".share-dialog");
+const closeButton = document.querySelector(".close-button");
+
+shareButton.addEventListener("click", (event) => {
+  if (navigator.share) {
+    navigator
+      .share({
+        title: "Bobby the Lamp",
+        url: "https://flow.page/bobbythelamp",
+      })
+      .then(() => {
+        console.log("Thanks for sharing!");
+      })
+      .catch(console.error);
+  } else {
+    shareDialog.classList.add("is-open");
+  }
+});
+
+closeButton.addEventListener("click", (event) => {
+  shareDialog.classList.remove("is-open");
+});
